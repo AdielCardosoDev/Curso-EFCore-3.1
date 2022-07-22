67fb2c0d4cb5a2bc7e7dfcf5a2a6b13425bc9894
Adiel Cardoso <62920887+AdielCardosoDev@users.noreply.github.com>
2022-07-21 22:21:39 -0300

    Metodos

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -5,6 +5,10 @@ namespace CursoEFCore.Data
 {
     public class ApplicationContext : DbContext
     {
+
+        public DbSet<Pedido> Pedido { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=ADIELCARDOSO\ESTUDOS;Database=EFCore;User Id=sa;Password=1234;");
--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using CursoEFCore.Domain;
+using CursoEFCore.ValueObjects;
 
 namespace CursoEFCore
 {
@@ -6,7 +8,30 @@ namespace CursoEFCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InserirDados();
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Descrição = "Produto Teste",
+                CodigoBarras = "1234567891231",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            using var db = new Data.ApplicationContext();
+            db.Produtos.Add(produto);
+            //db.Set<Produto>().Add(produto);
+            //não recomendado
+            //db.Entry(produto).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+            //db.Add(produto);
+
+            //salvando no banco
+            var registros = db.SaveChanges();
+            Console.WriteLine($"Total Registro(s): {registros}");
         }
     }
 }
