1295e507cf6116774a73daa07f7dd3349244c6d4
Adiel Cardoso <62920887+AdielCardosoDev@users.noreply.github.com>
2022-07-24 12:47:44 -0300

    Ajustes

--- Curso/Data/ApplicationContext.cs
+++ Curso/Data/ApplicationContext.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using CursoEFCore.Domain;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,12 +13,37 @@ namespace CursoEFCore.Data
         public DbSet<Cliente> Clientes { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=ADIELCARDOSO\ESTUDOS;Database=EFCore;User Id=sa;Password=1234;");
+            optionsBuilder.UseSqlServer(@"Server=ADIELCARDOSO\ESTUDOS;Database=EFCore;User Id=sa;Password=1234;",
+            p => p.EnableRetryOnFailure(//tentativas de conexão
+                 maxRetryCount: 2,
+                 maxRetryDelay: TimeSpan.FromSeconds(5),
+                 errorNumbersToAdd: null)
+                 .MigrationsHistoryTable("CursoEFCore"));// mudando o nome da tabela de migração
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+            MapearPropriedadesEsquecidas(modelBuilder);
+        }
+
+        private void MapearPropriedadesEsquecidas(ModelBuilder modelBuilder)
+        {
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                var properties = entity.GetProperties().Where(p => p.ClrType == typeof(string));
+
+                foreach (var property in properties)
+                {
+                    if (string.IsNullOrEmpty(property.GetColumnType())
+                    && !property.GetMaxLength().HasValue)
+                    {
+                        //property.SetMaxLength(100);
+                        property.SetColumnType("VARCHAR(100)");
+                    }
+                }
+            }
         }
 
 
